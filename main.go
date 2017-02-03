1a3f18a34627c2164f985816800b0bd96e1f3379
Mika <garfieldduck@gmail.com>
2017-02-03 17:10:31 +0800

    Add a secure middleware
    - use `github.com/unrolled/secure` to improve overall security

--- main.go
+++ main.go
@@ -4,11 +4,36 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/unrolled/secure"
 	"twreporter.org/go-api/routers"
 )
 
 func main() {
+	// security: no one can put it in an iframe
+	secureMiddleware := secure.New(secure.Options{
+		FrameDeny: true,
+	})
+	secureFunc := func() gin.HandlerFunc {
+		return func(c *gin.Context) {
+			err := secureMiddleware.Process(c.Writer, c.Request)
+
+			// If there was an error, do not continue.
+			if err != nil {
+				c.Abort()
+				return
+			}
+
+			// Avoid header rewrite if response is a redirection.
+			if status := c.Writer.Status(); status > 300 && status < 399 {
+				c.Abort()
+			}
+		}
+	}()
+
 	router := routers.SetupRouter()
+
+	router.Use(secureFunc)
 
 	s := &http.Server{
 		Addr:         ":8080",
