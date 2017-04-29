2d3326eed4e0d2cd61144f7da86086a64eab8cc8
nick <nickhsine97753017@gmail.com>
2017-04-29 19:22:47 +0800

    Update db.go
    Workaround for connecting to cloudsql

--- utils/db.go
+++ utils/db.go
@@ -8,6 +8,9 @@ import (
 
 // InitDB initiates the database connection
 func InitDB() (*gorm.DB, error) {
+	// WORKAROUND -- let process sleep to wait for cloud sql proxy.
+	time.Sleep(time.Duration(5) * time.Second)
+	
 	// connect to MySQL database
 	db, err := gorm.Open("mysql", Cfg.DBSettings.User+":"+Cfg.DBSettings.Password+"@tcp("+Cfg.DBSettings.Address+":"+Cfg.DBSettings.Port+")/"+Cfg.DBSettings.Name+"?parseTime=true")
 
