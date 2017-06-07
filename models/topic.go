5c7972b9d42bb60b46a1374dbfc6d3f2625edc36
nickhsine <nickhsine97753017@gmail.com>
2017-06-07 12:03:06 +0800

    Update topic.go. Add JSON field key

--- models/topic.go
+++ models/topic.go
@@ -29,27 +29,27 @@ type TopicMeta struct {
 type Topic struct {
 	ID                         bson.ObjectId   `json:"id" bson:"_id"`
 	Slug                       string          `json:"slug"`
-	TopicName                  string          `bson:"topic_name"`
-	Title                      string          `bson:"title"`
-	TitlePosition              string          `bson:"title_position,omitempty"`
-	Subtitle                   string          `bson:"subtitle"`
-	Headline                   string          `bson:"headline"`
-	State                      string          `bson:"state"`
+	TopicName                  string          `bson:"topic_name" json:"topic_name"`
+	Title                      string          `bson:"title" json:"title"`
+	TitlePosition              string          `bson:"title_position", json:"title_position"`
+	Subtitle                   string          `bson:"subtitle" json:"subtitle"`
+	Headline                   string          `bson:"headline" json:"headline"`
+	State                      string          `bson:"state" json:"state"`
 	PublishedDate              time.Time       `bson:"publishedDate" json:"published_date"`
-	Description                Brief           `bson:"description,omitempty"`
-	TeamDescription            Brief           `bson:"team_description,omitempty"`
+	Description                Brief           `bson:"description,omitempty" json:"description,omitempty"`
+	TeamDescription            Brief           `bson:"team_description,omitempty" json:"team_description,omitempty"`
 	Relateds                   []PostMeta      `bson:"-" json:",omitempty"`
 	RelatedsOrigin             []bson.ObjectId `bson:"relateds,omitempty" json:"-"`
-	RelatedsFormat             string          `bson:"relateds_format"`
-	RelatedsBackground         string          `bson:"relateds_background"`
+	RelatedsFormat             string          `bson:"relateds_format" json:"relateds_format"`
+	RelatedsBackground         string          `bson:"relateds_background" json:"relateds_background"`
 	LeadingImage               *Image          `bson:"-" json:"leading_image,omitempty"`
 	LeadingImageOrigin         bson.ObjectId   `bson:"leading_image,omitempty" json:"-"`
 	LeadingImagePortrait       *Image          `bson:"-" json:"leading_image_portrait,omitempty"`
 	LeadingImagePortraitOrigin bson.ObjectId   `bson:"leading_image_portrait,omitempty" json:"-"`
 	LeadingVideo               *Video          `bson:"-" json:"leading_video,omitempty"`
 	LeadingVideoOrigin         bson.ObjectId   `bson:"leading_video,omitempty" json:"-"`
-	OgTitle                    string          `bson:"og_title"`
-	OgDescription              string          `bson:"og_description"`
+	OgTitle                    string          `bson:"og_title" json:"og_title"`
+	OgDescription              string          `bson:"og_description" json:"og_description"`
 	OgImage                    *Image          `bson:"-" json:"og_image,omitempty"`
 	OgImageOrigin              bson.ObjectId   `bson:"og_image,omitempty" json:"-"`
 }
