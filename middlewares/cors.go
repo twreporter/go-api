5b7afcb2bbf5e7d647f7a8d8df9e63be72aaf411
nickhsine <nickhsine97753017@gmail.com>
2017-06-21 10:18:11 +0800

    Set Access-Control-Allow-Origin: *

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -10,7 +10,7 @@ import (
 // CORSMiddleware ...
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
