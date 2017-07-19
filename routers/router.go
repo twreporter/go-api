c3bfd71a54949d6752886a9155ce69e5e374c1fb
nickhsine <nickhsine97753017@gmail.com>
2017-07-19 10:58:16 +0800

    Add CORS in response header for production environment

--- routers/router.go
+++ routers/router.go
@@ -6,22 +6,26 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"twreporter.org/go-api/controllers"
+	"twreporter.org/go-api/utils"
 )
 
 // SetupRouter ...
 func SetupRouter(cf *controllers.ControllerFactory) *gin.Engine {
 	engine := gin.Default()
-	engine.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"https://twreporter.org", "https://dev.twreporter.org",
-			"https://www.twreporter.org", "http://twreporter.org", "http://www.twreporter.org",
-			"http://dev.twreporter.org", "http://localhost:3000"},
-		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "DELETE", "UPDATE"},
-		AllowHeaders: []string{"Origin", "Content-Type", "Authorization", "Accept",
-			"Accept-Encoding", "Client-Security-Token", "X-Requested-With", "x-access-token"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           30 * time.Minute,
-	}))
+
+	if utils.Cfg.Environment == "production" {
+		engine.Use(cors.New(cors.Config{
+			AllowOrigins: []string{"https://twreporter.org", "https://dev.twreporter.org",
+				"https://www.twreporter.org", "http://twreporter.org", "http://www.twreporter.org",
+				"http://dev.twreporter.org"},
+			AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "DELETE", "UPDATE"},
+			AllowHeaders: []string{"Origin", "Content-Type", "Authorization", "Accept",
+				"Accept-Encoding", "Client-Security-Token", "X-Requested-With", "x-access-token"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           30 * time.Minute,
+		}))
+	}
 
 	// Simple group: v1
 	routerGroup := engine.Group("/v1")
