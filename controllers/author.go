40896512dfaaa5814f375e6c1a6dad6940a64638
nickhsine <nickhsine97753017@gmail.com>
2018-08-01 10:51:48 +0800

    [feature] /v1/authors{?limit,offset,sort}[GET] endpoint for fetching authors

---
+++ controllers/author.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"twreporter.org/go-api/models"
+)
+
+// GetAuthors receive HTTP GET method request, and return the authors.
+// `limit`, `offset` and `sort` are the url query params,
+// which define the rule we retrieve authors from storage.
+func (nc *NewsController) GetAuthors(c *gin.Context) (int, gin.H, error) {
+	const defaultLimit = 20
+	const defaultSort = "updatedAt"
+	var authors []models.FullAuthor
+	var err error
+	var total int
+
+	_, limit, offset, sort, _ := nc.GetQueryParam(c)
+
+	if limit == 0 {
+		limit = defaultLimit
+	}
+
+	if sort == "" {
+		sort = defaultSort
+	}
+
+	authors, total, err = nc.Storage.GetFullAuthors(limit, offset, sort)
+
+	if err != nil {
+		return 0, gin.H{}, err
+	}
+
+	return http.StatusOK, gin.H{
+		"status": "success",
+		"data": gin.H{
+			"records": authors,
+			"meta": models.MetaOfResponse{
+				Total:  total,
+				Offset: offset,
+				Limit:  limit,
+			},
+		},
+	}, nil
+}
---
+++ storage/author.go
@@ -0,0 +1,65 @@
+package storage
+
+import (
+	"time"
+
+	"github.com/jinzhu/copier"
+	"gopkg.in/mgo.v2/bson"
+	"twreporter.org/go-api/models"
+	"twreporter.org/go-api/utils"
+	//	log "github.com/Sirupsen/logrus"
+)
+
+// GetFullAuthors finds the authors according to mongo aggregation pipeline stages
+func (m *MongoStorage) GetFullAuthors(limit int, offset int, sort string) ([]models.FullAuthor, int, error) {
+	type author struct {
+		ID       bson.ObjectId `bson:"_id"`
+		JobTitle string        `bson:"job_title"`
+		Name     string        `bson:"name"`
+		Bio      struct {
+			Html string `bson:"html"`
+			Md   string `bson:"md"`
+		}
+		Email      string              `bson:"email"`
+		Thumbnails []models.MongoImage `bson:"thumbnails"`
+		UpdatedAt  time.Time           `bson:"updatedAt"`
+	}
+
+	var authors []models.FullAuthor
+	var fa models.FullAuthor
+	var total int
+	var err error
+	var results []author
+	var result author
+
+	pipeline := []bson.M{
+		bson.M{"$sort": bson.M{sort: -1}},
+		bson.M{"$skip": offset},
+		bson.M{"$limit": limit},
+		bson.M{"$lookup": bson.M{"from": "images", "localField": "image", "foreignField": "_id", "as": "thumbnails"}},
+	}
+
+	collection := m.db.DB(utils.Cfg.MongoDBSettings.DBName).C("contacts")
+	if total, err = collection.Count(); err != nil {
+		return authors, 0, m.NewStorageError(err, "MongoStorage.GetFullAuthors", "can not get total count of authors")
+	}
+
+	pipe := collection.Pipe(pipeline)
+
+	if err = pipe.All(&results); err != nil {
+		return authors, 0, m.NewStorageError(err, "MongoStorage.GetFullAuthors", "can not get authors from storage")
+	}
+
+	// Copy fields/values from `author`s to `FullAuthor`s
+	for i := range results {
+		result = results[i]
+		fa = models.FullAuthor{}
+		copier.Copy(&fa, &result)
+		if len(result.Thumbnails) > 0 {
+			fa.Thumbnail = &result.Thumbnails[0]
+		}
+		authors = append(authors, fa)
+	}
+
+	return authors, total, nil
+}
