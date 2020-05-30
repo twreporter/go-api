19fa0d8539c1e6b9a30f6603ea990223b796ca31
Ching-Yang, Tseng <n7711245566@gmail.com>
2020-05-30 11:36:08 +0800

    api/news: adjust storage interface w.r.t query

--- news/controller.go
+++ news/controller.go
@@ -1,14 +1,18 @@
 package news
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
 
 type newsStorage interface {
-	GetMetaOfPosts() ([]Post, error)
-	GetFullPosts() ([]Post, error)
-	GetMetaOfTopics() ([]Topic, error)
-	GetFullOfTopics() ([]Topic, error)
+	GetPosts(*Query) ([]Post, error)
+	GetTopics(*Query) ([]Topic, error)
 
-	Count() (int, error)
+	GetPostCount(*Filter) (int, error)
+	GetTopicCount(*Filter) (int, error)
 }
 
 type newsController struct {
@@ -20,13 +24,75 @@ func NewController(s newsStorage) *newsController {
 }
 
 func (nc *newsController) GetPosts(c *gin.Context) {
+	q := NewQuery(FromUrlQueryMap(c.Request.URL.Query()))
+
+	posts, err := nc.Storage.GetPosts(q)
+
+	if err != nil {
+		log.Errorf("%+v", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	total, err := nc.Storage.GetPostCount(&q.Filter)
+	if err != nil {
+		log.Errorf("%+v", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "records": posts, "meta": gin.H{
+		"total":  total,
+		"offset": q.Offset,
+		"limit":  q.Limit,
+	}})
 }
 
 func (nc *newsController) GetAPost(c *gin.Context) {
+	q := NewQuery(FromSlug(c.Query("slug")))
+	q.Limit = 1
+
+	posts, err := nc.Storage.GetPosts(q)
+
+	if err != nil {
+		log.Errorf("%+v", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "records": posts[0]})
 }
 
 func (nc *newsController) GetTopics(c *gin.Context) {
+	q := NewQuery(FromUrlQueryMap(c.Request.URL.Query()))
+
+	topics, err := nc.Storage.GetTopics(q)
+
+	if err != nil {
+		log.Errorf("%+v", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	total, err := nc.Storage.GetTopicCount(&q.Filter)
+	if err != nil {
+		log.Errorf("%+v", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "records": topics, "meta": gin.H{
+		"total":  total,
+		"offset": q.Offset,
+		"limit":  q.Limit,
+	}})
 }
 
 func (nc *newsController) GetATopic(c *gin.Context) {
+	q := NewQuery(FromSlug(c.Query("slug")))
+	q.Limit = 1
+
+	topics, err := nc.Storage.GetTopics(q)
+
+	if err != nil {
+		log.Errorf("%+v", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "ok", "records": topics[0]})
 }
--- news/query.go
+++ news/query.go
@@ -1,42 +1,72 @@
 package news
 
-import "gopkg.in/guregu/null.v4"
+import (
+	"net/url"
+	"strconv"
+)
 
 type (
-	OrderBy struct {
-		IsAsc null.Bool
-	}
-
 	Pagination struct {
-		Offset uint
-		Limit  uint
+		Offset int
+		Limit  int
 	}
 
-	PostFilter struct {
-		Slug string
+	Filter struct {
+		Slug  string
+		State string
 	}
 
-	PostQuery struct {
+	Query struct {
 		Pagination
-		Filter PostFilter
-		Sort   PostSort
+		Filter Filter
+		Sort   string
+		Full   bool
 	}
 
-	PostSort struct {
-		UpdatedAt OrderBy
-	}
-
-	TopicFilter struct {
-		Slug string
-	}
-
-	TopicQuery struct {
-		Pagination
-		Filter TopicFilter
-		Sort   TopicSort
-	}
-
-	TopicSort struct {
-		UpdatedAt OrderBy
-	}
+	Options func(*Query)
 )
+
+func FromSlug(slug string) Options {
+	return func(q *Query) {
+		q.Filter.Slug = slug
+	}
+}
+
+func FromUrlQueryMap(u url.Values) Options {
+	return func(q *Query) {
+		offset, err := strconv.Atoi(u.Get("offset"))
+		if err != nil {
+			q.Offset = offset
+		}
+		limit, err := strconv.Atoi(u.Get("limitt"))
+		if err != nil {
+			q.Limit = limit
+		}
+
+		full, err := strconv.ParseBool(u.Get("full"))
+		if err != nil {
+			q.Full = full
+		}
+	}
+}
+
+func NewQuery(options ...Options) *Query {
+	q := &Query{
+		Pagination: Pagination{Offset: 0, Limit: 10},
+		Sort:       "-publishedDate",
+	}
+
+	for _, o := range options {
+		o(q)
+	}
+	return q
+}
+
+func (q *Query) SetPagination(offset, limit int) {
+	q.Offset = offset
+	q.Limit = limit
+}
+
+func (q *Query) SetSort(sort string) {
+	q.Sort = sort
+}
