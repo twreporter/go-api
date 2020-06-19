82878636d76d4e3ac6cb43d6277b2327798e7b65
Ching-Yang, Tseng <n7711245566@gmail.com>
2020-06-19 16:45:05 +0800

    api/news: refactor post list query
    This patch refactors post list query with the supported query parameters
    to internal/news/.

--- internal/news/query.go
+++ internal/news/query.go
@@ -30,6 +30,12 @@ type SortBy struct {
 	UpdatedAt     query.Order
 }
 
+const (
+	sortByPublishedDate = "published_date"
+	sortByUpdatedAt     = "updated_at"
+	descending          = "-"
+)
+
 func ParseSinglePostQuery(c *gin.Context) *Query {
 	var q Query
 
@@ -39,6 +45,50 @@ func ParseSinglePostQuery(c *gin.Context) *Query {
 
 	if full, err := strconv.ParseBool(c.Query("full")); err == nil {
 		q.Full = full
+	}
+	return &q
+}
+
+func ParsePostListQuery(c *gin.Context) *Query {
+	var q Query
+
+	defaultQuery := Query{
+		Pagination: query.Pagination{Offset: 0, Limit: 10},
+		Sort:       SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(false)}},
+	}
+
+	q = defaultQuery
+	// Parse filter
+	if len(c.QueryArray("category_id")) > 0 {
+		q.Filter.Categories = c.QueryArray("category_id")
+	}
+	if len(c.QueryArray("tag_id")) > 0 {
+		q.Filter.Tags = c.QueryArray("tag_id")
+	}
+	if len(c.QueryArray("id")) > 0 {
+		q.Filter.Tags = c.QueryArray("id")
+	}
+
+	// Parse pagination
+	if offset, err := strconv.Atoi(c.Query("offset")); err == nil {
+		q.Offset = offset
+	}
+	if limit, err := strconv.Atoi(c.Query("limit")); err == nil {
+		q.Limit = limit
+	}
+
+	// Parse sorting
+	if sort := c.Query("sort"); sort != "" {
+		switch sort {
+		case sortByPublishedDate:
+			q.Sort = SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(true)}}
+		case descending + sortByPublishedDate:
+			q.Sort = SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(false)}}
+		case sortByUpdatedAt:
+			q.Sort = SortBy{UpdatedAt: query.Order{IsAsc: null.BoolFrom(true)}}
+		case descending + sortByUpdatedAt:
+			q.Sort = SortBy{UpdatedAt: query.Order{IsAsc: null.BoolFrom(false)}}
+		}
 	}
 	return &q
 }
--- internal/news/query_test.go
+++ internal/news/query_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"gopkg.in/guregu/null.v3"
+	"twreporter.org/go-api/internal/query"
 )
 
 func TestParseSinglePostQuery(t *testing.T) {
@@ -44,6 +46,92 @@ func TestParseSinglePostQuery(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			c := helperCreateContext(t, tc.url, "/:slug")
 			got := ParseSinglePostQuery(c)
+			if !reflect.DeepEqual(*got, *tc.want) {
+				t.Errorf("expected query %v, got %v", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestParsePostListQuery(t *testing.T) {
+	cases := []struct {
+		name string
+		url  string
+		want *Query
+	}{
+		{
+			name: "Given default parameter",
+			url:  "http://example.com/posts",
+			want: &Query{
+				Pagination: query.Pagination{Offset: 0, Limit: 10},
+				Sort:       SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(false)}},
+			},
+		},
+		{
+			name: "Given the category_id parameter",
+			url:  "http://example.com/posts?category_id=cid1&category_id=cid2",
+			want: &Query{
+				Pagination: query.Pagination{Offset: 0, Limit: 10},
+				Filter:     Filter{Categories: []string{"cid1", "cid2"}},
+				Sort:       SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(false)}},
+			},
+		},
+		{
+			name: "Given the tag_id parameter",
+			url:  "http://example.com/posts?tag_id=tid1&tag_id=tid2",
+			want: &Query{
+				Pagination: query.Pagination{Offset: 0, Limit: 10},
+				Filter:     Filter{Tags: []string{"tid1", "tid2"}},
+				Sort:       SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(false)}},
+			},
+		},
+		{
+			name: "Given the id parameter",
+			url:  "http://example.com/posts?id=id1&id=id2",
+			want: &Query{
+				Pagination: query.Pagination{Offset: 0, Limit: 10},
+				Filter:     Filter{Tags: []string{"id1", "id2"}},
+				Sort:       SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(false)}},
+			},
+		},
+		{
+			name: "Given the sort by published_date parameter",
+			url:  "http://example.com/posts?sort=published_date",
+			want: &Query{
+				Pagination: query.Pagination{Offset: 0, Limit: 10},
+				Sort:       SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(true)}},
+			},
+		},
+		{
+			name: "Given the sort by updated_at parameter",
+			url:  "http://example.com/posts?sort=-updated_at",
+			want: &Query{
+				Pagination: query.Pagination{Offset: 0, Limit: 10},
+				Sort:       SortBy{UpdatedAt: query.Order{IsAsc: null.BoolFrom(false)}},
+			},
+		},
+		{
+			name: "Given the pagination parameters",
+			url:  "http://example.com/posts?offset=5&limit=6",
+			want: &Query{
+				Pagination: query.Pagination{Offset: 5, Limit: 6},
+				Sort:       SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(false)}},
+			},
+		},
+		{
+			name: "Given query parameters, ignore unsupported one",
+			url:  "http://example.com/posts?unsupported=value",
+			want: &Query{
+				Pagination: query.Pagination{Offset: 0, Limit: 10},
+				Sort:       SortBy{PublishedDate: query.Order{IsAsc: null.BoolFrom(false)}},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			c := helperCreateContext(t, tc.url, "/posts")
+			got := ParsePostListQuery(c)
 			if !reflect.DeepEqual(*got, *tc.want) {
 				t.Errorf("expected query %v, got %v", tc.want, got)
 			}
