14aac66ef78d91a7c857e3e6921a56085a4231c9
Aylie Chou <aylie_chou@twreporter.org>
2022-01-11 18:28:01 +0800

    fix: fix syntax error

--- migrations/000008_update_donation_redesign_table_schemas.up.sql
+++ migrations/000008_update_donation_redesign_table_schemas.up.sql
@@ -4,7 +4,7 @@ SET `send_receipt` = CASE `send_receipt`
                      WHEN 'no' THEN 'no_receipt'
                      WHEN 'yearly' THEN 'paperback_receipt_by_year'
                      WHEN 'monthly' THEN 'paperback_receipt_by_month'
-                   END
+                   END;
 ALTER TABLE `pay_by_prime_donations` MODIFY `send_receipt` enum('no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
 
 ALTER TABLE `periodic_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no', 'no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
@@ -13,5 +13,5 @@ SET `send_receipt` = CASE `send_receipt`
                      WHEN 'no' THEN 'no_receipt'
                      WHEN 'yearly' THEN 'paperback_receipt_by_year'
                      WHEN 'monthly' THEN 'paperback_receipt_by_month'
-                   END
+                   END;
 ALTER TABLE `periodic_donations` MODIFY `send_receipt` enum('no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
