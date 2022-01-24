c60442832de798287a1dea51678e04f5284d2a78
Aylie Chou <aylie_chou@twreporter.org>
2022-01-24 13:37:43 +0800

    fix: add field update & preserve to-be-drop fields for new-old compatibility

--- migrations/000008_update_donation_redesign_table_schemas.down.sql
+++ migrations/000008_update_donation_redesign_table_schemas.down.sql
@@ -1,17 +1,19 @@
-ALTER TABLE `pay_by_prime_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no', 'no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
 UPDATE `pay_by_prime_donations`
 SET `send_receipt` = CASE `send_receipt`
-                     WHEN 'no_receipt' THEN 'no'
-                     WHEN 'paperback_receipt_by_year' THEN 'yearly'
-                     WHEN 'paperback_receipt_by_month' THEN 'monthly'
-                   END
-ALTER TABLE `pay_by_prime_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no');
+                       WHEN 'no_receipt' THEN 'no'
+                       WHEN 'paperback_receipt_by_year' THEN 'yearly'
+                       WHEN 'paperback_receipt_by_month' THEN 'monthly'
+                     END,
+`notes` = `cardholder_words_for_twreporter`,
+`cardholder_national_id` = `cardholder_security_id`;
+ALTER TABLE `pay_by_prime_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no') DEFAULT 'yearly';
 
-ALTER TABLE `periodic_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no', 'no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
 UPDATE `periodic_donations`
 SET `send_receipt` = CASE `send_receipt`
-                     WHEN 'no_receipt' THEN 'no'
-                     WHEN 'paperback_receipt_by_year' THEN 'yearly'
-                     WHEN 'paperback_receipt_by_month' THEN 'monthly'
-                   END
-ALTER TABLE `periodic_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no');
+                       WHEN 'no_receipt' THEN 'no'
+                       WHEN 'paperback_receipt_by_year' THEN 'yearly'
+                       WHEN 'paperback_receipt_by_month' THEN 'monthly'
+                     END,
+`notes` = `cardholder_words_for_twreporter`,
+`cardholder_national_id` = `cardholder_security_id`;
+ALTER TABLE `periodic_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no') DEFAULT 'yearly';
--- migrations/000008_update_donation_redesign_table_schemas.up.sql
+++ migrations/000008_update_donation_redesign_table_schemas.up.sql
@@ -1,17 +1,19 @@
 ALTER TABLE `pay_by_prime_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no', 'no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
 UPDATE `pay_by_prime_donations`
 SET `send_receipt` = CASE `send_receipt`
-                     WHEN 'no' THEN 'no_receipt'
-                     WHEN 'yearly' THEN 'paperback_receipt_by_year'
-                     WHEN 'monthly' THEN 'paperback_receipt_by_month'
-                   END;
-ALTER TABLE `pay_by_prime_donations` MODIFY `send_receipt` enum('no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
+                       WHEN 'no' THEN 'no_receipt'
+                       WHEN 'yearly' THEN 'paperback_receipt_by_year'
+                       WHEN 'monthly' THEN 'paperback_receipt_by_month'
+                     END,
+`cardholder_words_for_twreporter` = `notes`,
+`cardholder_security_id` = `cardholder_national_id`;
 
 ALTER TABLE `periodic_donations` MODIFY `send_receipt` enum('yearly', 'monthly', 'no', 'no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
 UPDATE `periodic_donations`
 SET `send_receipt` = CASE `send_receipt`
-                     WHEN 'no' THEN 'no_receipt'
-                     WHEN 'yearly' THEN 'paperback_receipt_by_year'
-                     WHEN 'monthly' THEN 'paperback_receipt_by_month'
-                   END;
-ALTER TABLE `periodic_donations` MODIFY `send_receipt` enum('no_receipt', 'digital_receipt_by_month', 'digital_receipt_by_year', 'paperback_receipt_by_month', 'paperback_receipt_by_year');
+                       WHEN 'no' THEN 'no_receipt'
+                       WHEN 'yearly' THEN 'paperback_receipt_by_year'
+                       WHEN 'monthly' THEN 'paperback_receipt_by_month'
+                     END,
+`cardholder_words_for_twreporter` = `notes`,
+`cardholder_security_id` = `cardholder_national_id`;
