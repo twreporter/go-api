b6c58d91be68f62a803ec76407a34c613c7ad244
Lucien <liruchen0302@gmail.com>
2023-04-21 15:51:40 +0800

    feat: use current timestamp for updated_at and created_at

--- migrations/000009_create_roles_table.up.sql
+++ migrations/000009_create_roles_table.up.sql
@@ -2,12 +2,12 @@ CREATE TABLE IF NOT EXISTS `roles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `name_en` varchar(50) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `roles` (`name`, `name_en`, `created_at`, `updated_at`) VALUES ('探索者', 'explorer', now(), now());
-INSERT INTO `roles` (`name`, `name_en`, `created_at`, `updated_at`) VALUES ('行動者', 'action taker', now(), now());
-INSERT INTO `roles` (`name`, `name_en`, `created_at`, `updated_at`) VALUES ('開創者', 'trailblazer', now(), now());
+INSERT INTO `roles` (`name`, `name_en`) VALUES ('探索者', 'explorer');
+INSERT INTO `roles` (`name`, `name_en`) VALUES ('行動者', 'action taker');
+INSERT INTO `roles` (`name`, `name_en`) VALUES ('開創者', 'trailblazer');
