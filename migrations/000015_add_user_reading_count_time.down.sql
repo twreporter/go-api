61545bdcab0d83356a3b2d867fd603bb8feeefbf
Aylie Chou <aylie_chou@twreporter.org>
2023-11-23 16:34:46 +0800

    fix: update migration 15
    - add users.read_posts_count
    - add users.read_posts_sec

--- migrations/000015_add_user_reading_count_time.down.sql
+++ migrations/000015_add_user_reading_count_time.down.sql
@@ -1,5 +1,7 @@
 -- drop column
-ALTER TABLE `users` DROP agree_data_collection;
+ALTER TABLE `users` DROP `agree_data_collection`;
+ALTER TABLE `users` DROP `read_posts_count`;
+ALTER TABLE `users` DROP `read_posts_sec`;
 
 -- drop tables
 DROP TABLE IF EXISTS `users_posts_reading_count`;
