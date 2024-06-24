292a9ceceb090db899d37e9ef9cbf64945db5d1a
Howar31 <howar31@gmail.com>
2024-06-25 01:38:48 +0800

    feat: Add receipt number column for periodic donations

--- migrations/000020_add_receipt_no_column.down.sql
+++ migrations/000020_add_receipt_no_column.down.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `pay_by_prime_donations`
 DROP COLUMN `receipt_number`;
+
+ALTER TABLE `periodic_donations`
+DROP COLUMN `receipt_number`;
--- migrations/000020_add_receipt_no_column.up.sql
+++ migrations/000020_add_receipt_no_column.up.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `pay_by_prime_donations`
 ADD COLUMN `receipt_number` varchar(12) DEFAULT NULL;
+
+ALTER TABLE `periodic_donations`
+ADD COLUMN `receipt_number` varchar(12) DEFAULT NULL;
