d856988ae4d94b8cd0d3d304a4f9b93569badb56
Aylie Chou <aylie_chou@twreporter.org>
2024-11-02 17:23:12 +0800

    fix: incorrect `host` value

--- internal/member_cms/graphql.go
+++ internal/member_cms/graphql.go
@@ -131,7 +131,7 @@ func (c *Client) runWithJSON(ctx context.Context, req *Request, resp interface{}
 	}
 	c.logf(">> headers: %v", r.Header)
 	if r.Host != "" {
-		req.Host = r.Host
+		r.Host = req.Host
 		c.logf(">> hosts: %s", r.Host)
 	}
 	r = r.WithContext(ctx)
